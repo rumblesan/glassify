e8b795b4abb17282bbf8b2be4ed52b5dde2d1067
Guy John <guy@rumblesan.com>
2017-01-17 14:42:21 +0000

    Tweaking grid creation a bit

--- src/app/Section.js
+++ src/app/Section.js
@@ -15,7 +15,7 @@ const midpoint = (p1, p2) => {
   const xdiff = p2.x - p1.x;
   const ydiff = p2.y - p1.y;
   //const r = gaussianRand();
-  const r = 0.3;
+  const r = 0.5;
 
   return new paper.Point(p1.x + (xdiff * r), p1.y + (ydiff * r));
 };
